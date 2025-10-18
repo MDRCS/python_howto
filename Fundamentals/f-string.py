7f965da1f9e51695a74a8eeef4734325aab089ad
MDRCS <elrahali.md@gmail.com>
2025-10-18 20:56:42 +0200

    F-String Formating with specifiers.

--- Fundamentals/f-string.py
+++ Fundamentals/f-string.py
@@ -18,9 +18,25 @@ if __name__ == "__main__":
     tasks = {"name" : "Laundry", "urgency": 3}
     assert f"Tasks: {tasks}" == "Tasks: {'name': 'Laundry', 'urgency': 3}"
 
+    # Specifiers - Structure text display
     tasks_ids = [1, 2, 3]
     tasks_names = ["Laundry", "Homework", "Night Walk"]
     tasks_urgency = [3, 2, 1]
-
+    center = '*^24' # center specifier
     for i in range(3):
-        print(f"{tasks_ids[i]:^12} {tasks_names[i]:^12} {tasks_urgency[i]:^12}")
+        print(f"{tasks_ids[i]:{center}} {tasks_names[i]:{center}} {tasks_urgency[i]:{center}}")
+    
+    # Right-Left with Star * as a Padding
+    """
+        specifier = '^12'   # center spaces specifier
+        specifier = '*^12'  # center stars specifier
+        specifier = '>3'    # left spaces specifier
+        specifier = '<3'    # right spaces specifier
+        specifier = '*<3'   # right stars specifier
+    """
+    left = '*>3'
+    right = '*<3'
+    for i in range(3):
+        print(f"{tasks_ids[i]:{left}} {tasks_names[i]:{center}} {tasks_urgency[i]:{right}}")
+
+    
